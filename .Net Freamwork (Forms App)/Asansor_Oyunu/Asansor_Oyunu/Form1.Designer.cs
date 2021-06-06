61e145cac3b1e48dfb3462e04c68ca3a43f530b3
Ferhat <79553498+FerhatCengz@users.noreply.github.com>
2021-06-06 19:15:38 +0300

    Asansor Oyunu

---
+++ .Net Freamwork (Forms App)/Asansor_Oyunu/Asansor_Oyunu/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace Asansor_Oyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.kat1 = new System.Windows.Forms.Button();
+            this.kat3 = new System.Windows.Forms.Button();
+            this.kat5 = new System.Windows.Forms.Button();
+            this.kat6 = new System.Windows.Forms.Button();
+            this.kat4 = new System.Windows.Forms.Button();
+            this.kat2 = new System.Windows.Forms.Button();
+            this.acilldurumdurha = new System.Windows.Forms.Button();
+            this.zkat = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label8 = new System.Windows.Forms.Label();
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // kat1
+            // 
+            this.kat1.Location = new System.Drawing.Point(38, 113);
+            this.kat1.Name = "kat1";
+            this.kat1.Size = new System.Drawing.Size(94, 71);
+            this.kat1.TabIndex = 0;
+            this.kat1.Text = "1.Kat";
+            this.kat1.UseVisualStyleBackColor = true;
+            this.kat1.Click += new System.EventHandler(this.kat1_Click);
+            // 
+            // kat3
+            // 
+            this.kat3.Location = new System.Drawing.Point(38, 190);
+            this.kat3.Name = "kat3";
+            this.kat3.Size = new System.Drawing.Size(94, 71);
+            this.kat3.TabIndex = 1;
+            this.kat3.Text = "3.Kat";
+            this.kat3.UseVisualStyleBackColor = true;
+            this.kat3.Click += new System.EventHandler(this.kat3_Click);
+            // 
+            // kat5
+            // 
+            this.kat5.Location = new System.Drawing.Point(38, 267);
+            this.kat5.Name = "kat5";
+            this.kat5.Size = new System.Drawing.Size(94, 71);
+            this.kat5.TabIndex = 2;
+            this.kat5.Text = "5.Kat";
+            this.kat5.UseVisualStyleBackColor = true;
+            this.kat5.Click += new System.EventHandler(this.kat5_Click);
+            // 
+            // kat6
+            // 
+            this.kat6.Location = new System.Drawing.Point(138, 267);
+            this.kat6.Name = "kat6";
+            this.kat6.Size = new System.Drawing.Size(94, 71);
+            this.kat6.TabIndex = 6;
+            this.kat6.Text = "6.Kat";
+            this.kat6.UseVisualStyleBackColor = true;
+            this.kat6.Click += new System.EventHandler(this.kat6_Click);
+            // 
+            // kat4
+            // 
+            this.kat4.Location = new System.Drawing.Point(138, 190);
+            this.kat4.Name = "kat4";
+            this.kat4.Size = new System.Drawing.Size(94, 71);
+            this.kat4.TabIndex = 5;
+            this.kat4.Text = "4.Kat";
+            this.kat4.UseVisualStyleBackColor = true;
+            this.kat4.Click += new System.EventHandler(this.kat4_Click);
+            // 
+            // kat2
+            // 
+            this.kat2.Location = new System.Drawing.Point(138, 113);
+            this.kat2.Name = "kat2";
+            this.kat2.Size = new System.Drawing.Size(94, 71);
+            this.kat2.TabIndex = 4;
+            this.kat2.Text = "2.Kat";
+            this.kat2.UseVisualStyleBackColor = true;
+            this.kat2.Click += new System.EventHandler(this.kat2_Click);
+            // 
+            // acilldurumdurha
+            // 
+            this.acilldurumdurha.Location = new System.Drawing.Point(6, 344);
+            this.acilldurumdurha.Name = "acilldurumdurha";
+            this.acilldurumdurha.Size = new System.Drawing.Size(194, 71);
+            this.acilldurumdurha.TabIndex = 9;
+            this.acilldurumdurha.Text = "Acil Durum";
+            this.acilldurumdurha.UseVisualStyleBackColor = true;
+            this.acilldurumdurha.Click += new System.EventHandler(this.acilldurumdurha_Click);
+            // 
+            // zkat
+            // 
+            this.zkat.Location = new System.Drawing.Point(38, 36);
+            this.zkat.Name = "zkat";
+            this.zkat.Size = new System.Drawing.Size(194, 71);
+            this.zkat.TabIndex = 8;
+            this.zkat.Text = "Z Kat";
+            this.zkat.UseVisualStyleBackColor = true;
+            this.zkat.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Gray;
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.acilldurumdurha);
+            this.groupBox1.Controls.Add(this.kat1);
+            this.groupBox1.Controls.Add(this.zkat);
+            this.groupBox1.Controls.Add(this.kat3);
+            this.groupBox1.Controls.Add(this.kat6);
+            this.groupBox1.Controls.Add(this.kat5);
+            this.groupBox1.Controls.Add(this.kat4);
+            this.groupBox1.Controls.Add(this.kat2);
+            this.groupBox1.Location = new System.Drawing.Point(798, 44);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(289, 441);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kat İşlemleri";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 388);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 38);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "1.Kat";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 311);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 38);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "2.Kat";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 38);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "4.Kat";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 234);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 38);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "3.Kat";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 38);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "6.Kat";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 80);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 38);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "5.Kat";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(3, 471);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(152, 38);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Zemin Kat";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(220, 450);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(94, 60);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(226, 361);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 38);
+            this.label8.TabIndex = 10;
+            // 
+            // timer4
+            // 
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Orange;
+            this.ClientSize = new System.Drawing.Size(1089, 542);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button kat1;
+        private System.Windows.Forms.Button kat3;
+        private System.Windows.Forms.Button kat5;
+        private System.Windows.Forms.Button kat6;
+        private System.Windows.Forms.Button kat4;
+        private System.Windows.Forms.Button kat2;
+        private System.Windows.Forms.Button acilldurumdurha;
+        private System.Windows.Forms.Button zkat;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Timer timer4;
+    }
+}
+
---
+++ .Net Freamwork (Forms App)/Asansor_Oyunu/Asansor_Oyunu/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+namespace Asansor_Oyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int zeminkat = 0;
+        int katbir = 0;
+        int katiki = 0;
+        int katuc = 0;
+        int katdort = 0;
+        private void kat1_Click(object sender, EventArgs e)
+        {
+            timer2.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Point zemin = new Point();
+            zeminkat++;
+            zemin.X = 220;
+            zemin.Y = 450;
+            pictureBox1.Location = zemin;
+            if (pictureBox1.Location == zemin)
+            {
+                timer1.Stop();
+                MessageBox.Show("Z Katındasın");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Point birincikat = new Point();
+            birincikat.X = 220;
+            birincikat.Y = 366;
+            pictureBox1.Location = birincikat;
+            if (pictureBox1.Location == birincikat)
+            {
+                timer2.Stop();
+                MessageBox.Show("1.Kattasın");
+            }
+        }
+
+        private void kat2_Click(object sender, EventArgs e)
+        {
+            Point ikincikat = new Point();
+            ikincikat.X = 220;
+            ikincikat.Y = 295;
+            pictureBox1.Location = ikincikat;
+            if (pictureBox1.Location == ikincikat)
+            {
+                MessageBox.Show("2.Kattasın");
+            }
+        }
+
+        private void kat3_Click(object sender, EventArgs e)
+        {
+            Point ucuncukat = new Point();
+            ucuncukat.X = 220;
+            ucuncukat.Y = 204;
+            pictureBox1.Location = ucuncukat;
+            if (pictureBox1.Location == ucuncukat)
+            {
+                MessageBox.Show("3.Kattasın");
+            }
+        }
+
+        private void kat4_Click(object sender, EventArgs e)
+        {
+            Point dorduncukat = new Point();
+            dorduncukat.X = 220;
+            dorduncukat.Y = 137;
+            pictureBox1.Location = dorduncukat;
+            if (pictureBox1.Location == dorduncukat)
+            {
+                MessageBox.Show("4.Kattasınız");
+            }
+        }
+
+        private void kat5_Click(object sender, EventArgs e)
+        {
+            Point besıncıkat = new Point();
+            besıncıkat.X = 220;
+            besıncıkat.Y = 65;
+            pictureBox1.Location = besıncıkat;
+            if (pictureBox1.Location == besıncıkat)
+            {
+                MessageBox.Show("5.Kattasınız");
+            }
+        }
+
+        private void kat6_Click(object sender, EventArgs e)
+        {
+            Point altıncıkat = new Point();
+            altıncıkat.X = 220;
+            altıncıkat.Y = 5;
+            pictureBox1.Location = altıncıkat;
+            if (pictureBox1.Location == altıncıkat)
+            {
+                MessageBox.Show("6.Kattasınız");
+            }
+        }
+
+        private void acilldurumdurha_Click(object sender, EventArgs e)
+        {
+            WindowsMediaPlayer wmp = new WindowsMediaPlayer();
+            wmp.URL = "C:\\Users\\User\\OneDrive\\Masaüstü\\Png Resimler\\a.mp3";
+            wmp.controls.play();
+            kat1.Enabled = false;
+            kat2.Enabled = false;
+            kat3.Enabled = false;
+            kat4.Enabled = false;
+            kat5.Enabled = false;
+            kat6.Enabled = false;
+            zkat.Enabled = false;
+            timer3.Start();
+
+
+        }
+        int sayac = 7;
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            timer4.Start();
+            sayac--;
+            label8.Text = sayac.ToString();
+            if (sayac == 0)
+            {
+                timer3.Stop();
+                sayac = 7;
+                kat1.Enabled = true;
+                kat2.Enabled = true;
+                kat3.Enabled = true;
+                kat4.Enabled = true;
+                kat5.Enabled = true;
+                kat6.Enabled = true;
+                zkat.Enabled = true;
+                timer4.Stop();
+                this.BackColor = Color.Orange;
+                label8.Text = " ";
+            }
+            
+                
+                
+            
+
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            if (sayac % 2 == 0)
+            {
+                this.BackColor = Color.Red;
+            }
+            else if (sayac % 2 == 1)
+            {
+                this.BackColor = Color.DarkRed;
+            }
+
+        }
+
+        
+    }
+}
