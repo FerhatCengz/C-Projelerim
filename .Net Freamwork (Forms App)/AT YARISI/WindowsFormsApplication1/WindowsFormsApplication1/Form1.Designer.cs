aaf63f82b5cc3d40ab808311faee1b5eb92e8b21
Ferhat <79553498+FerhatCengz@users.noreply.github.com>
2021-06-06 17:35:55 +0300

    At Yarışı Oyun Projesi

---
+++ .Net Freamwork (Forms App)/AT YARISI/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(118, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(101, 90);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(118, 140);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(101, 90);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(118, 253);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(101, 90);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Red;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(930, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(78, 374);
+            this.panel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(18, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 200);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "F\r\nI\r\nN\r\nS\r\nH";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 27);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Fidan";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 27);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Gül";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1, 253);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 27);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Papatya";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 111;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Location = new System.Drawing.Point(1044, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(319, 396);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sonuçlar";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 26;
+            this.listBox1.Location = new System.Drawing.Point(6, 33);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(310, 368);
+            this.listBox1.TabIndex = 11;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(338, 399);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(28, 34);
+            this.textBox1.TabIndex = 12;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(372, 399);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(29, 35);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "+";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(304, 399);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(28, 35);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightGreen;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.Menu;
+            this.button1.Location = new System.Drawing.Point(419, 388);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 54);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Başlat";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CadetBlue;
+            this.ClientSize = new System.Drawing.Size(1385, 463);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ .Net Freamwork (Forms App)/AT YARISI/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void restart()
+        {
+
+            Point hourse = new Point();
+            hourse.X = 118;
+            hourse.Y = 28;
+            pictureBox1.Location = hourse;
+
+            Point hourse2 = new Point();
+            hourse2.X = 118;
+            hourse2.Y = 140;
+            pictureBox2.Location = hourse2;
+
+            Point hourse3 = new Point();
+            hourse3.X = 118;
+            hourse3.Y = 253;
+            pictureBox3.Location = hourse3;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            if (textBox1.Text == "0" || textBox1.Text == "")
+            {
+                MessageBox.Show("Oyun Başlatılamadı\nTur Sayısı 0 ' dan Büyük Olmalı");
+            }
+            else
+            {
+                goplay();
+            }
+        }
+        int sayac = 6;
+        int tur = 0;
+
+        public void goplay()
+        {
+            tur++;
+
+            int x = 0, x2 = 0, x3 = 0;
+            Random rastgele = new Random();
+            x = rastgele.Next(100, 824);
+            x2 = rastgele.Next(100, 824);
+            x3 = rastgele.Next(100, 824);
+
+            Point hourse = new Point();
+            hourse.X = x;
+            hourse.Y = 28;
+            pictureBox1.Location = hourse;
+
+            Point hourse2 = new Point();
+            hourse2.X = x2;
+            hourse2.Y = 140;
+            pictureBox2.Location = hourse2;
+
+            Point hourse3 = new Point();
+            hourse3.X = x3;
+            hourse3.Y = 253;
+            pictureBox3.Location = hourse3;
+
+            if (x > x2 && x > x3)
+            {
+                MessageBox.Show("Fidan Kazandı", "WİN", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                timer1.Start();
+                timer2.Start();
+                button1.Visible = true;
+                listBox1.Items.Add(tur + ".Tur Fidan");
+
+            }
+            else if (x2 > x && x2 > x3)
+            {
+                MessageBox.Show("Gül Kazandı", "WİN", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                timer1.Start();
+                timer2.Start();
+                listBox1.Items.Add(tur + ".Tur Gül");
+
+            }
+            else if (x3 > x && x3 > x)
+            {
+                MessageBox.Show("Papatya Kazandı", "WİN", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                timer1.Start();
+                timer2.Start();
+                listBox1.Items.Add(tur + ".Tur Papatya");
+
+            }
+            else
+            {
+                MessageBox.Show("BERABERE", "Sonuç", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                timer1.Start();
+                timer2.Start();
+                listBox1.Items.Add(tur + ".Tur Berabere");
+
+            }
+
+            if (tur == bitis)
+            {
+                int sifir = 0;
+                timer1.Start();
+                timer2.Start();
+                textBox1.Text = sifir.ToString();
+                listBox1.Items.Clear();
+                textBox1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                bitis = 0;
+                tur = 0;
+
+            }
+            if (bitis == 0)
+            {
+                timer1.Stop();
+                timer2.Stop();
+                restart();
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            sayac--;
+            button1.Text = ("Restart" + "(" + sayac + ")");
+            if (sayac == 0)
+            {
+                timer1.Stop();
+                restart();
+                button1.Enabled = true;
+                button1.Text = "Başlat";
+                button1.ForeColor = Color.White;
+                button1.BackColor = Color.LightGreen;
+
+                sayac = 6;
+            }
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button1.ForeColor = Color.Red;
+            button1.BackColor = Color.DarkRed;
+            timer2.Stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int sifir = 0;
+            textBox1.Text = sifir.ToString();
+            button3.Visible = false;
+            button2.Visible = true;
+        }
+
+        int bitis = 0;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bitis++;
+            textBox1.Text = bitis.ToString();
+            button3.Visible = true;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            bitis--;
+            textBox1.Text = bitis.ToString();
+            if (bitis == 0)
+            {
+                button3.Visible = false;
+            }
+
+        }
+
+    }
+}
